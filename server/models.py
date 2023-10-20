e8ca0c7bddfdfb7e622a69d3f6f28a9f75d6df66
tobias <tobias.omondi@student.moringaschool.com>
2023-10-20 22:46:56 +0300

    not done

--- server/models.py
+++ server/models.py
@@ -11,13 +11,24 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday =db.Column('birthday',db.String)
+    birthday = db.Column(db.Date, nullable=False)
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(20), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=True)
+    animals = db.relationship('Animal', backref='enclosure', lazy=True)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'), nullable=False)
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'), nullable=False)
